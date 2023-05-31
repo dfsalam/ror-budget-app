21562dbfb3e1c4fb3a22ff934a8e3e138614c14d
dfsalam <dfsalam@gmail.com>
2023-05-30 19:14:31 -0500

    Add references between users-expenses and users-groups.

--- db/migrate/20230530225920_create_expenses.rb
+++ db/migrate/20230530225920_create_expenses.rb
@@ -1,6 +1,7 @@
 class CreateExpenses < ActiveRecord::Migration[7.0]
   def change
     create_table :expenses do |t|
+      t.references :author, index: true, foreign_key: {to_table: :users, primary_key: :id}
       t.string :name
 
       t.timestamps
--- db/migrate/20230530230000_create_groups.rb
+++ db/migrate/20230530230000_create_groups.rb
@@ -1,6 +1,7 @@
 class CreateGroups < ActiveRecord::Migration[7.0]
   def change
     create_table :groups do |t|
+      t.references :author, index: true, foreign_key: {to_table: :users, primary_key: :id}
       t.string :name
       t.string :icon
 
