dcbc8b597c40a1d743c60e1814fc21878adc33ed
dfsalam <dfsalam@gmail.com>
2023-05-30 23:29:24 -0500

    Setup devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :expense_groups
   resources :groups
   resources :expenses
@@ -6,5 +7,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "groups#index"
 end
