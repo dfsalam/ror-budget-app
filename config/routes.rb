e3baaf2b69d8cbc27a327238b2a6aa95302efba6
dfsalam <dfsalam@gmail.com>
2023-06-01 18:39:11 -0500

    Add splash view and style login page.

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  get '/splash', to:'users#splash', as: 'splash'  
   root "groups#index"
 end
