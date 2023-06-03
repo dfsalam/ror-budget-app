4558854b3a54dc0368e9e7206dd46aa1bf19e3dd
dfsalam <dfsalam@gmail.com>
2023-06-02 20:38:50 -0500

    Add group model tests.

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,13 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe Group, type: :model do  
+RSpec.describe Group, type: :model do
+  describe 'The group returns correct name and icon.' do
+    let(:user) { User.new(name: 'Diego', email: 'test@test.com') }
+    let(:group) { Group.new(name: 'Utilities', icon:'asdfg', author_id:user.id)}    
+    it 'the group has the correct name' do
+      expect(group.name).to eq('Utilities')
+    end
+    it 'the group has the correct icon' do
+      expect(group.icon).to eq('asdfg')
+    end
+
+  end
   describe 'should not be blank.' do
-    it 'Name' do
-      group = Group.new(name: nil, icon:'qqq')
+    let(:user) { User.new(name: 'Diego', email: 'test@test.com') }
+    let(:group) { Group.new(name: nil, icon:'asdfg', author_id:user.id)}
+    it 'Name' do      
       expect(group).to_not be_valid
     end
-    it 'Icon' do
-      group = Group.new(name: 'qqq', icon: nil)
+  end
+  describe 'should not be blank' do
+    let(:user) { User.new(name: 'Diego', email: 'test@test.com') }
+    let(:group) { Group.new(name: 'Utilities', icon: nil, author_id:user.id)}
+    it 'Icon' do     
       expect(group).to_not be_valid
     end   
   end
