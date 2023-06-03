833b9504c1da58095dd7a0ac02ebdfb568eb885f
dfsalam <dfsalam@gmail.com>
2023-06-02 19:05:03 -0500

    Fix login problem.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :groups do
     resources :expense_groups, only: [:new, :index, :create]
   end
-  resources :users
+  #resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")  
