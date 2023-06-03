1574f5cea83ff3a42cb1c1025c7028ad892dbc15
dfsalam <dfsalam@gmail.com>
2023-06-02 20:27:03 -0500

    Add user model tests.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do  
+RSpec.describe User, type: :model do
+  describe 'The user returns correct name and email.' do
+    let(:user) { User.new(name: 'Diego', email: 'test@test.com') }
+    it 'the User has the correct name' do
+      expect(user.name).to eq('Diego')
+    end
+    it 'returns User has the correct email' do
+      expect(user.email).to eq('test@test.com')
+    end
+  end
   describe 'Name' do
-    it 'should not be blank.' do
-      user = User.new(name: nil)
+    let(:user) { User.new(name: nil, email: 'test@test.com') }
+    it 'should not be blank.' do     
       expect(user).to_not be_valid
     end 
   end
